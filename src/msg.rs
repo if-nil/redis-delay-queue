4f7367c60d12c4a95511d510cb369fc9c64ed47b
baba <ifnil.git@gmail.com>
2023-12-29 16:05:21 +0800

    替换SystemTime序列化结果

--- src/msg.rs
+++ src/msg.rs
@@ -1,6 +1,6 @@
-use std::time::SystemTime;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
-use serde::Serialize;
+use serde::{Serialize, Serializer};
 use uuid::Uuid;
 
 use crate::Mode;
@@ -10,8 +10,42 @@ pub(crate) struct Msg {
     pub(crate) id: String,
     pub(crate) queue_name: String,
     pub(crate) msg: String,
+    #[serde(serialize_with = "serialize_time")]
     pub(crate) delay_time: SystemTime,
     pub(crate) mode: Mode,
+}
+
+fn serialize_time<S>(time: &SystemTime, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let duration_since_epoch = match time.duration_since(UNIX_EPOCH) {
+        Ok(duration_since_epoch) => duration_since_epoch,
+        Err(_) => Duration::ZERO,
+    };
+    serializer.serialize_u64(duration_since_epoch.as_secs())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        ops::Add,
+        time::{Duration, SystemTime},
+    };
+
+    use super::Msg;
+
+    #[test]
+    fn test_msg_serialize() {
+        let msg = Msg::new(
+            "queue_name".to_string(),
+            "msg".to_string(),
+            SystemTime::now().add(Duration::from_secs(10)),
+            crate::Mode::P2P,
+        );
+        let msg_json = serde_json::to_string(&msg).unwrap();
+        println!("{msg_json}");
+    }
 }
 
 impl Msg {
